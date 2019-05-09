6512cf8ea07e4e2f2869c4904cf9eca2a5d79bf6
Pleszkán Tamás <rb1trq@INF.ELTE.HU>
2019-05-09 18:39:03 +0200

    Some changes

--- 2017bead_osztott_domino/eclipse/2017bead_osztott_domino/src/DominoMain.java
+++ 2017bead_osztott_domino/eclipse/2017bead_osztott_domino/src/DominoMain.java
@@ -1,4 +1,15 @@
 
 public class DominoMain {
+	public static void main(String[] args) {
+		new Thread(() -> {
+			DominoServer.main("a", "b", "c");
+		}).start();
 
+		Thread.sleep(100);
+		
+		new Thread(() -> {
+			DominoClient.main("a", "b", "c");
+			DominoClient.main("a b c".split(" "));
+		}).start();
+	}
 }
